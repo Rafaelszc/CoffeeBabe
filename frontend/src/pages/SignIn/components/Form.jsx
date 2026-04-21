f8304df5616c11dd60a2ab353416cf573cf7529e
Rafaelszc <rafaelbjj84@gmail.com>
2026-04-21 15:02:11 -0300

    feat (frontend): add signin page

---
+++ frontend/src/pages/SignIn/components/Form.jsx
@@ -0,0 +1,61 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { signInSchema } from "@/schemas/userSchema"
+import { Coffee, Eye, EyeClosed } from "lucide-react"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+
+export const Form = () => {
+  const { register, handleSubmit, reset, formState: { errors } } = useForm({resolver: zodResolver(signInSchema)})
+  const [seePassword, setSeePassword] = useState(false)
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data))
+    reset()
+  }
+
+  return (
+    <section className="flex items-center justify-center py-16 bg-primary/40">
+      <div className="w-full max-w-md bg-white p-10 rounded-4xl shadow-sm">
+        <div className="flex flex-col items-center gap-2 mb-10">
+          <Coffee className="text-secondary" />
+          <h2 className="text-3xl text-secondary-foreground font-semibold" >Sign In</h2>
+          <p className="text-muted-foreground" >Access your account</p>
+        </div>
+        <div>
+          <form action={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+            <div className="flex flex-col gap-3">
+              <Label htmlFor="email">E-mail</Label>
+              <Input id="email" type="email" {...register("email")} className="bg-secondary/10 py-5" />
+              <p className="text-red-500 text-sm min-h-5">
+                {errors.email?.message}
+              </p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label htmlFor="password">Senha</Label>
+              <div className="relative">
+                {seePassword 
+                ? <Eye onClick={() => setSeePassword(!seePassword)} className="absolute right-3 top-1/2 -translate-y-1/2 h-4 cursor-pointer"/> 
+                : <EyeClosed onClick={() => setSeePassword(!seePassword)} className="absolute right-3 top-1/2 -translate-y-1/2 h-4 cursor-pointer"/>}
+
+                <Input id="password" type={seePassword ? "text" : "password"} {...register("password")} className="bg-secondary/10 py-5" />
+              </div>
+              <p className="text-red-500 text-sm min-h-5">
+                {errors.password?.message}
+              </p>
+            </div>
+            
+            <div className="flex flex-col items-center gap-4">
+              <Button className="cursor-pointer bg-secondary w-full rounded-full py-5 hover:bg-secondary/80 transition-colors duration-300" type="submit">Sign in</Button>
+              <span className="text-sm text-secondary">New to CoffeeBabe? <Link to="/signup" className="text-blue-700 hover:text-blue-400 transition-colors duration-300">Create an account</Link></span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
+}
