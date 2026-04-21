00dc870980e66138fa731f54ee93c8b821a88fab
Rafaelszc <rafaelbjj84@gmail.com>
2026-04-21 15:01:41 -0300

    refactor (frontend): change layout logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,20 @@ import { Home } from './pages/Home/Home'
 import { Products } from './pages/Products/Products'
 import { Checkout } from './pages/Checkout/Checkout'
 import { NotFound } from './pages/NotFound/NotFound'
+import { Layout } from './components/site/Layout'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/checkout' element={<Checkout />} />
-        <Route path="/*" element={<NotFound />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path="/*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,13 @@
 import { BottomNavbar } from '@/components/site/BottomNavbar'
-import { Navbar } from '../../components/site/Navbar'
 import { BestSellers } from './components/BestSellers'
 import { HeroSection } from './components/HeroSection'
-import { Footer } from '@/components/site/Footer'
 
 export const Home = () => {
   return (
     <div>
-      <Navbar />
       <HeroSection />
       <BestSellers />
       <BottomNavbar />
-      <Footer />
     </div>
   )
 }
