8f19bcf529463a1d3bb8d1098dddc61bb291c64e
Rafaelszc <rafaelbjj84@gmail.com>
2026-05-01 17:12:35 -0300

    feat (frontend): add toast instance in site layout and in products cards

--- frontend/src/components/site/Layout.jsx
+++ frontend/src/components/site/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import { Navbar } from "./Navbar"
 import { Footer } from "./Footer"
+import { Toaster } from "sonner"
 
 export const Layout = () => {
     return (
         <div className="min-h-screen flex flex-col bg-primary">
             <Navbar />
             <main className="flex-1">
+                <Toaster />
                 <Outlet />
             </main>
             <Footer />
--- frontend/src/components/site/ProductCard.jsx
+++ frontend/src/components/site/ProductCard.jsx
@@ -1,19 +1,21 @@
 import { Plus } from "lucide-react"
 import { Button } from "../ui/button"
+import { addToCart } from "@/api/productService"
 
-export const ProductCard = ({ id, imagePath, title, description, price }) => {
+export const ProductCard = ({ id, name, image, price }) => {
     const handleAddToCart = () => {
-        console.log(`Added product ${id} to cart`);
+        const product = {id, name, image, price}
+        addToCart(product)
     }
 
     return (
         <article style={{height: "500px", width: "350px"}} className="overflow-hidden flex flex-col gap-5 rounded-2xl bg-white shadow-md m-5 pb-5 hover:scale-105 hover:shadow-lg transition-all duration-500 group">
             <div className="overflow-hidden">
-                <img className="rounded-t-2xl group-hover:scale-110 delay-75 transition-transform duration-500" src={imagePath} alt={title} />
+                <img className="rounded-t-2xl group-hover:scale-110 delay-75 transition-transform duration-500" src={image.url} alt={name} />
             </div>
             <div className="px-5 flex flex-col gap-2">
-                <h3 className="text-xl">{title}</h3>
-                <p className="text-sm text-secondary">{description}</p>
+                <h3 className="text-xl">{name}</h3>
+                <p className="text-sm text-secondary">{image.alt}</p>
             </div>
             <div className="flex items-center justify-between px-5">
                 <h3 className="text-lg font-light text-secondary tracking-wide">$ {price.toFixed(2)}</h3>
