fe42a0a08197a4e3c1f304baa4fec5cbc8570f99
Rafaelszc <rafaelbjj84@gmail.com>
2026-05-05 20:16:29 -0300

    refactor (backend): set image url as unique value

--- backend/src/main/resources/db/migration/V2__create-table-products.sql
+++ backend/src/main/resources/db/migration/V2__create-table-products.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS products(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(15) NOT NULL UNIQUE,
-    image_url TEXT NOT NULL,
+    image_url TEXT NOT NULL UNIQUE,
     description TEXT NOT NULL,
     price REAL NOT NULL
 );
