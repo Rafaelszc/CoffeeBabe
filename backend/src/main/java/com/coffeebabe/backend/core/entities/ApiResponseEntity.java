3bf0385d865440a69ed171ba858e206dd06030a8
Rafaelszc <rafaelbjj84@gmail.com>
2026-05-05 12:35:39 -0300

    fix (backend): fix product_id foreign key reference

--- backend/src/main/java/com/coffeebabe/backend/core/entities/ApiResponseEntity.java
+++
@@ -1,8 +0,0 @@
-package com.coffeebabe.backend.core.entities;
-
-public class ApiResponseEntity<T> {
-    private boolean success;
-    private String message;
-    private T data;
-    private Object errors;
-}
