8d883eb32292ef40e7a4052453bb4782262cfc4f
Rafaelszc <rafaelbjj84@gmail.com>
2026-05-06 14:23:45 -0300

    refactor (backend): add email validation in User entitie

--- backend/src/main/java/com/coffeebabe/backend/core/entities/User.java
+++ backend/src/main/java/com/coffeebabe/backend/core/entities/User.java
@@ -1,6 +1,9 @@
 package com.coffeebabe.backend.core.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,10 +20,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Size(min = 3, max = 30, message = "Username must be at least 3 characters and most 30 characters")
     private String username;
 
+    @Email(message = "Invalid email")
     private String email;
 
+    @Size(min = 6, max = 30, message = "Password must be at least 6 characters and most 30 characters")
     private String password;
 
     @Enumerated(EnumType.STRING)
